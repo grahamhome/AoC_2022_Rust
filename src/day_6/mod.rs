6ca6cc8907b52aa9cf2cbb7d26b86fd4c74ad6da
Graham Home <grahamhome333@gmail.com>
2022-12-06 14:35:56 -0500

    Day 6

--- src/day_6/mod.rs
+++ src/day_6/mod.rs
@@ -3,7 +3,7 @@ use std::collections::HashSet;
 use crate::config::Config;
 
 pub fn run(config: Config) {
-    let mut input: Vec<char> = fs::read_to_string(config.file_path).expect("Unable to read input file").chars().collect();
+    let input: Vec<char> = fs::read_to_string(config.file_path).expect("Unable to read input file").chars().collect();
     let mut index = config.count;
     let mut win_set: HashSet<char> = HashSet::from_iter(input[index-config.count..index].iter().cloned());
     while win_set.len() < config.count {
