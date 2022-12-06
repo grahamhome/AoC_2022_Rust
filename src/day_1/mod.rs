e7b13873ecb79b9e0efa91f319c460f9417bcd9d
Graham Home <grahamhome333@gmail.com>
2022-12-06 12:26:28 -0500

    Move Day 1 functions into module

---
+++ src/day_1/mod.rs
@@ -0,0 +1,24 @@
+use std::fs;
+use crate::Config;
+
+pub fn run(config: Config) {
+    let mut elves: Vec<i32> = Vec::new();
+    let mut index = 0;
+    elves.push(0);
+
+    let lines = fs::read_to_string(config.file_path).expect("Unable to read input file");
+    for line in lines.split("\n") {
+        match str::is_empty(str::trim(line)) {
+            true => {
+                index += 1;
+                elves.push(0);
+            },
+            false => {
+                elves[index] += str::trim(line).parse::<i32>().unwrap();
+            }
+        }
+    }
+    elves.sort();
+    println!("{}", elves.iter().max().unwrap());
+    println!("{:?}", elves[elves.len()-3..].iter().sum::<i32>());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,14 @@
 use std::env;
-use std::fs;
+
+mod day_1;
+
 fn main() {
     let config = parse_config();
+    day_1::run(config)
 
-    let mut elves: Vec<i32> = Vec::new();
-    let mut index = 0;
-    elves.push(0);
-
-    let lines = fs::read_to_string(config.file_path).expect("Unable to read input file");
-    for line in lines.split("\n") {
-        match str::is_empty(str::trim(line)) {
-            true => {
-                index += 1;
-                elves.push(0);
-            },
-            false => {
-                elves[index] += str::trim(line).parse::<i32>().unwrap();
-            }
-        }
-    }
-    elves.sort();
-    println!("{}", elves.iter().max().unwrap());
-    println!("{:?}", elves[elves.len()-3..].iter().sum::<i32>());
 }
 
-struct Config {
+pub struct Config {
     file_path: String
 }
 
